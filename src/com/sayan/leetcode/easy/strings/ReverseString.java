80a2ef53bf9b1ae57599ce03160ae93d759efd41
Sayan Guha <sayanguha99@gmail.com>
2021-07-03 12:58:00 +0530

    LC 344: Reverse String

---
+++ src/com/sayan/leetcode/easy/strings/ReverseString.java
@@ -0,0 +1,28 @@
+package com.sayan.leetcode.easy.strings;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int start=0;
+        reverseCharArray(s,start,s.length-1);
+    }
+    private void reverseCharArray(char[] s,int start,int end){
+        char temp='\0';
+        if(start>=end){return;}
+        temp=s[end];
+        s[end]=s[start];
+        s[start]=temp;
+        reverseCharArray(s,start+1,end-1);
+    }
+    private static void printArray(char[] s){
+        for(char c:s){
+            System.out.print(c+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        ReverseString obj = new ReverseString();
+        char[] s = new char[]{'h','e','l','l','o'};
+        obj.reverseString(s);
+        printArray(s);
+    }
+}
