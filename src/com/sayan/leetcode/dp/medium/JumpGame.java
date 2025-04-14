9755ca27b78a884678c8c18f668c0f52ce6ff7c4
Sayan Guha <sayanguha99@gmail.com>
2025-04-14 16:43:24 +0530

    Update Minimum jumps problem to include recursive and memoized solution
    Update Minimum jumps problem to include recursive and memoized solution

--- src/com/sayan/leetcode/dp/medium/JumpGame.java
+++ src/com/sayan/leetcode/dp/medium/JumpGame.java
@@ -2,10 +2,36 @@ package com.sayan.leetcode.dp.medium;
 
 public class JumpGame {
     public boolean canJump(int[] nums){
-        int[] memo = new int[nums.length+1];
-        for(int i:memo)
+        boolean canJumpRecursive = canJumpHelperRecursive(nums, 0)==1;
+        System.out.println("Can jump recursive output: "+canJumpRecursive);
+
+        int[] memo = new int[nums.length];
+        for(int i=0;i<memo.length;i++)
             memo[i]=-1;
-        return canJumpHelper(memo, nums, 0) == 1;
+        boolean canJumpMemoized = canJumpHelper(memo, nums, 0)==1;
+        System.out.println("Can jump memoized output: "+canJumpMemoized);
+
+        return canJumpMemoized;
+    }
+
+    private int canJumpHelperRecursive(int[] nums, int currIdx) {
+        
+        // If we reach the last index, we can jump to the end
+        if (currIdx == nums.length - 1) return 1;
+
+        // If we reach an index with 0, we cannot jump further
+        if (nums[currIdx] == 0) return 0;
+
+        for(int i=1;i<=nums[i];i++){
+            // Check if we can jump to the next index
+            // and if we can reach the end from there
+            // We are using currIdx+i<nums.length to avoid ArrayIndexOutOfBoundsException
+            if(currIdx+i<nums.length && canJumpHelperRecursive(nums, currIdx+i)==1)
+                return 1;           
+        }
+
+        // If we reach here, it means we cannot jump to the end from this index
+        return 0;
     }
 
     private int canJumpHelper(int[] memo, int[] nums, int currIdx) {
@@ -14,40 +40,16 @@ public class JumpGame {
 
         if (memo[currIdx] != -1) return memo[currIdx];
 
-        int canReachIdx = Math.min(currIdx+nums[currIdx],nums.length-1);
-
-        for(int jump=currIdx+1;jump<=canReachIdx;jump++){
-            if(canJumpHelper(memo,nums,jump)==1)
+        for(int i=1;i<=nums[currIdx];i++){
+            if(currIdx+i<nums.length && canJumpHelper(memo, nums, currIdx+i)==1)
                 return memo[currIdx]=1;
-                //return true;
-            //memo[currIdx]=canJumpHelper(memo,nums,jump)==1?1:0;
         }
-
-        //return false;
-        //memo[currIdx]=false;
-
-        //memo[currIdx]=0;
         return memo[currIdx]=0;
     }
 
-
-
-    /*private boolean canJumpHelper(boolean[] memo, int[] nums, int currIdx, int endIdx){
-        if(currIdx==endIdx) return true;
-        if(nums[currIdx]==0) return false;
-
-        if(memo[currIdx]) return memo[currIdx];
-        //boolean isPossible = false;
-        for(int jump=currIdx+1;jump<=nums[currIdx];jump++) {
-            if (currIdx + jump <= endIdx)
-                memo[currIdx] = memo[currIdx] || canJumpHelper(memo,nums, currIdx + jump, endIdx);
-        }
-        return memo[currIdx];
-    }*/
-
     public static void main(String[] args) {
         JumpGame obj = new JumpGame();
-        System.out.println(obj.canJump(new int[]{2,3,1,1,4}));
+        //System.out.println(obj.canJump(new int[]{2,3,1,1,4}));
         System.out.println(obj.canJump(new int[]{3,2,1,0,4}));
         System.out.println(obj.canJump(new int[]{1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9}));
     }
