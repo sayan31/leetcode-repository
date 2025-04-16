b6cf67accd70a5cc0bd48ddd561ae265fd1d8326
Sayan Guha <sayanguha99@gmail.com>
2025-04-16 21:19:06 +0530

    MinimumJumps space optimized solution
    space optimized solution

--- src/com/sayan/leetcode/dp/medium/MinimumJumps.java
+++ src/com/sayan/leetcode/dp/medium/MinimumJumps.java
@@ -8,7 +8,37 @@ public class MinimumJumps {
     }
 
     private static int minJumps(int[] arr) {
-        return minJumpsHelperRecursive(arr, 0);
+        int ans = minJumpsHelperRecursive(arr, 0);
+        System.out.println("Minimum jumps required (recursive): " + ans);
+
+        int ansSpaceOptimized = minJumpsHelperSpaceOptimizedDP(arr);
+        System.out.println("Minimum jumps required (space optimized): " + ansSpaceOptimized);
+        //return ansSpaceOptimized;
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+
+
+    }
+
+    private static int minJumpsHelperSpaceOptimizedDP(int[] arr) {
+        int n = arr.length;
+        if (n == 0 || arr[0] == 0) return -1; // If the array is empty or the first element is 0, we cannot jump
+
+        int jumps = 0; // Number of jumps made
+        int currEnd = 0; // The farthest index we can reach with the current number of jumps
+        int currFarthest = 0; // The farthest index we can reach with the next jump
+
+        for (int i = 0; i < n - 1; i++) {
+            currFarthest = Math.max(currFarthest, i + arr[i]); // Update the farthest index we can reach
+
+            if (i == currEnd) { // If we have reached the end of the current jump
+                jumps++; // Increment the number of jumps made
+                currEnd = currFarthest; // Update the end of the current jump
+
+                if (currEnd >= n - 1) break; // If we can reach or exceed the last index, break out of the loop
+            }
+        }
+
+        return currEnd >= n - 1 ? jumps : -1; // If we can reach or exceed the last index, return the number of jumps made, otherwise return -1
     }
 
     private static int minJumpsHelperRecursive(int[] arr, int currIdx) {
@@ -17,7 +47,7 @@ public class MinimumJumps {
         if (currIdx == arr.length - 1) return 0;
 
         // If we reach an index with 0, we cannot jump further
-        if (arr[currIdx] == 0) return -1;
+        if (arr[currIdx] == 0) return 0;
 
         int minJumps = Integer.MAX_VALUE;
         for (int i = 1; i <= arr[currIdx]; i++) {
